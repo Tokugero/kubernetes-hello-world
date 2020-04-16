70f3e48761fba55557de2adb9146c156f4b2f635
tokugero <tokugero@k8s-m01.home>
2020-04-15 17:10:11 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kubernetes-hello-world
 Simple sample application with some opinionated applications tack
+
+https://github.com/helm/charts/tree/master/stable/postgresql
---
+++ app/myapp.py
@@ -0,0 +1,37 @@
+from flask import Flask, Response
+import os
+import psycopg2
+
+app = Flask(__name__)
+query = ("select * from actor order by random() limit 5;")
+host = os.getenv("host")
+password = os.getenv("PSQL_PASSWORD")
+
+def callpsql():
+    conn = psycopg2.connect(dbname="postgres", user="postgres", password=password, host=host, port="5432")
+    cur = conn.cursor()
+    cur.execute(query)
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return results
+
+@app.route('/')
+def index():
+    response = callpsql()
+    print("Getting some juicy logs")
+    results = "I've sold to these famous people from server "+os.getenv("HOSTNAME")+": <br>\n"
+    for row in response:
+        results = results+"<br>\n"+str(row[1])+"\t"+str(row[2])
+    return results 
+
+@app.route('/health')
+def health():
+    f = open("/tmp/healthy", "r")
+    print(f)
+    health = f.readline().rstrip()
+    resp = Response(health)
+    if health != "healthy":
+        resp.status=500
+    resp.headers['health-check'] = health
+    return resp 
